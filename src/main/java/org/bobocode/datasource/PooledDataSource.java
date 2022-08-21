9b1940c5f752b7e38fa229c93ea713e3a0ffc5e2
AndriiTsepukh <andrii.tsepukh@icloud.com>
2022-08-22 01:37:40 +0300

    Update logic to make PooledDataSource vendor-agnostic.

--- src/main/java/org/bobocode/datasource/PooledDataSource.java
+++ src/main/java/org/bobocode/datasource/PooledDataSource.java
@@ -1,27 +1,26 @@
 package org.bobocode.datasource;
 
-import org.postgresql.ds.PGSimpleDataSource;
-
+import javax.sql.DataSource;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
 
-public class PooledDataSource extends PGSimpleDataSource {
+public class PooledDataSource implements DataSource {
     Queue<Connection> connectionsQueue = new LinkedList<>();
+    DataSource dataSource;
     private final int MAX_CONNECTIONS = 8;
-    public PooledDataSource(String url, String userName, String password){
+    public PooledDataSource(Supplier<DataSource> supplier){
         super();
+        dataSource = supplier.get();
         for (int i=0; i < MAX_CONNECTIONS; i++){
-            Connection connection = null;
             try {
-                super.setURL(url);
-                super.setUser(userName);
-                super.setPassword(password);
-                connection = super.getConnection();
-
+                var connection = dataSource.getConnection();
                 ConnectionProxy connectionProxy = new ConnectionProxy(connection, connectionsQueue);
-
                 connectionsQueue.add(connectionProxy);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
@@ -32,5 +31,46 @@ public class PooledDataSource extends PGSimpleDataSource {
     @Override
     public Connection getConnection() throws SQLException {
         return connectionsQueue.peek();
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return dataSource.getConnection(username, password);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return dataSource.getLogWriter();
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        dataSource.setLogWriter(out);
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        dataSource.setLoginTimeout(seconds);
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return dataSource.getLoginTimeout();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return dataSource.getParentLogger();
+    }
+
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return dataSource.unwrap(iface);
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return dataSource.isWrapperFor(iface);
     }
 }
